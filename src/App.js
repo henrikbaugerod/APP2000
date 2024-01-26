a67f2101b57126741fe229a3297285eaf0073a6d
Henrik Baugerød <henrikbaugerod@hotmail.com>
2024-01-26 12:10:06 +0100

    Basic layout

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
+import Home from "./Pages/Home";
 
 function App() {
     return (
         <div className="App">
-            <h1>Welcome to Catch Billiard</h1>
+            <Home />
         </div>
     );
 }
