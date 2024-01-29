75510f72bf11fbc745f2a4616493fcc731684ad9
bergisen <morten.bergmann4@gmail.com>
2024-01-29 16:08:05 +0100

    Lagt inn addPlayers versjon 1

--- src/Pages/NewPlayer.js
+++ src/Pages/NewPlayer.js
@@ -13,11 +13,33 @@ const NewPlayer = () => {
                 <div className="col-4">
                     <div className='profilePic'>
                         <img src="./images/person.jpg"  alt="Profile Picture" className="rounded-circle borderCircle" />
-
                     </div>
                 </div>
             </div>
-            
+            <div className='row mt-5'>
+                <div className='col-12'>
+                    <div className='textBox'>
+                        <input type="text" id="name" name="name" placeholder="Name" className='mb-3 rounded-3'></input>
+                        <input type="text" id="nickName" name="nickName" placeholder="Nickname" className='rounded-3'></input>
+                    </div>
+                </div>
+            </div>
+            <div className='row mt-5'>
+                <div className='col-12'>
+                    <div className='header1'>
+                        <h1>Category</h1>
+                            <div className='checkBox'>
+                                <input class="form-check-input" type="checkbox" id="catch" name="catch" value="catch"/>
+                                <label class="form-check-label" for="catch">Catch</label>
+                            </div>
+                            <div className='checkBox'>
+                            <input class="form-check-input" type="checkbox" id="external" name="external" value="external"/>
+                                <label class="form-check-label" for="external">External</label>
+                            </div>
+                    </div>
+                </div>
+            </div>
+
         </div>
     );
 };
