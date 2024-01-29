e082bdd241b5fea32a01c5cde4f4b50b74e09b21
bergisen <morten.bergmann4@gmail.com>
2024-01-29 13:15:55 +0100

    La inn profilbilde på AddPlayer.js
    La inn profilbilde på AddPlayer.js

--- src/Pages/NewPlayer.js
+++ src/Pages/NewPlayer.js
@@ -7,7 +7,17 @@ const NewPlayer = () => {
         <div className="container">
             <Header 
                 backLink={'/players'}
-            />            
+            />  
+
+            <div className="row justify-content-center">
+                <div className="col-4">
+                    <div className='profilePic'>
+                        <img src="./images/person.jpg"  alt="Profile Picture" className="rounded-circle borderCircle" />
+
+                    </div>
+                </div>
+            </div>
+            
         </div>
     );
 };
