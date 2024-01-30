84e96eec5c0477ab81c9ed9488847686bc3c51bb
bergisen <morten.bergmann4@gmail.com>
2024-01-30 09:29:27 +0100

    d

---
+++ src/Pages/Surprise.js
@@ -0,0 +1,14 @@
+const Surprise = () => {
+
+    return (
+        <div className="container">
+            <Header 
+                backLink={'/players'}
+            />  
+            <h1>Hei</h1>
+
+        </div>
+    );
+};
+
+export default NewPlayer;
