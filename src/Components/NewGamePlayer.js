8085ae84636faf550e11bd446e7dc9347fb3c86a
kulmixip <anders_b96@hotmail.com>
2024-02-09 12:17:26 +0100

    NewGame buttons and useState

--- src/Components/NewGamePlayer.js
+++ src/Components/NewGamePlayer.js
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const NewGamePlayer = () => {
+  const [findPlayer, setFindPlayer] = useState(false);
+
+  const handleClick = () => {
+    console.log("Link clicked");
+    sessionStorage.setItem('findPlayer', true)
+  };
+
   return (
     <div className="col-5">
       {/* Picture */}
       <div className="row mt-2 mb-2">
         <div className="col-12">
-          <div className="playerCircle bg-purple"></div>
+          <div
+            className="playerCircle bg-purple d-flex justify-content-center align-items-center"
+            onClick={handleClick}
+          >
+            <Link to="/Players">
+              <img src="./images/Vector.svg" style={{ width: "50px" }} />
+            </Link>
+          </div>
         </div>
       </div>
 
