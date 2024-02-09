9f0ba07a90794ca208fac1984030096241099a60
Henrik Baugerød <henrikbaugerod@hotmail.com>
2024-02-09 11:52:17 +0100

    Surpruse prototyp save

--- src/Pages/Surprise.js
+++ src/Pages/Surprise.js
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from '../Components/Header';
 
 const Surprise = () => {
+    const [challenge1, setChallenge1] = useState('None');
+    const [challenge2, setChallenge2] = useState('None');
+
+    const handleButtonClick = () => {
+        const challenges = [
+            "Ting 1",
+            "Ting 2",
+            "Ting 3"
+        ]
+
+        // Setting the challenge for each player
+        setChallenge1(challenges[generateRandomNumber(challenges.length)]);
+        setChallenge2(challenges[generateRandomNumber(challenges.length)]);
+    }
+
+    const generateRandomNumber = (max) => {
+        return Math.floor(Math.random() * max);
+    }
 
     return (
         <div className="container">
@@ -9,40 +27,29 @@ const Surprise = () => {
                 backLink={'/menu'}
             />
 
-            <div className="machine">
-                <div className="wrap">
-                    <div className="column">
-                        <div id="machine1" className="optionContainer">
-                            <span className="option">
-                                <span>Example A</span>
-                            </span>
-                            <span className="option">
-                                <span>Example B</span>
-                            </span>
-                        </div>
-                    </div>
-                    <div className="column">
-                        <div id="machine2" className="optionContainer">
-                            <span className="option">
-                                <span>Example C</span>
-                            </span>
-                            <span className="option">
-                                <span>Example D</span>
-                            </span>
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="mashupButton">
-                            <button id="randomizeButton" className="button" onClick={handleClick}>Go</button>
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div id="results">
-
-                        </div>
-                    </div>
+            <div className="row">
+                <div className="col-6 text-center">
+                    <h2>Player 1</h2>
+                    <p className="spinning">{challenge1}</p>
+                </div>
+                <div className="col-6 text-center">
+                    <h2>Player 2</h2>
+                    <p className="spinning">{challenge2}</p>
                 </div>
             </div>
+
+            <div className="row justify-content-center mt-5">
+                <div className="col-auto">
+                    <button className="btn button bg-darkPurple text-white justify-content-center py-3 rounded-pill" onClick={handleButtonClick}>Spin the wheel</button>
+                </div>
+            </div>
+
+            <div class="slots spinning">
+                <div class="window">
+                    <div class="outer-col"><div class="colen">dwada</div></div>
+                </div>
+            </div>
+            <input type="button" onclick="spin(this)" value="spin" />
         </div>
     );
 
