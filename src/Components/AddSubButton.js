2baa5e9999a9f8f4e3e13c268623a728f725e51a
kulmixip <anders_b96@hotmail.com>
2024-04-01 15:31:42 +0200

    Pointssystem

--- src/Components/AddSubButton.js
+++ src/Components/AddSubButton.js
@@ -7,16 +7,19 @@ const AddSubButton = (props) => {
 
   const setPlayer1Points = props.onSetPlayer1Points;
   const setPlayer2Points = props.onSetPlayer2Points;
-  const player1Points = props.player1Points;
-  const player2Points = props.player2Points;
   const currentPoints = props.currentPoints;
 
   const setPlayer1GainedPoints = props.setPlayer1GainedPoints;
   const setPlayer2GainedPoints = props.setPlayer2GainedPoints;
 
+  const player2Points = props.player2Points;
+  const player1Points = props.player1Points;
+
+  const p1Points = sessionStorage.getItem("player1poeng");
+  const p2Points = sessionStorage.getItem("player2poeng");
+
   const handleClick = () => {
     let score;
-    setPoints();
 
     // Logikk for når man klikker +/-
     if (add) {
@@ -42,47 +45,43 @@ const AddSubButton = (props) => {
         setPlayer1Points(score);
       }
     }
+
+    setPoints();
   };
 
   // Poengsystem
   const setPoints = () => {
     let difference = null;
     let winner = null;
-    let p1Points = null;
-    let p2Points = null;
+    let pointDifference = 0;
+    let p1Pot;
+    let p2Pot;
+
+    // Differanse (finn poengskille)
+    if (p1Points > p2Points) {
+      pointDifference = p1Points - p2Points;
+      p1Pot = 2 + 1 * Math.floor(pointDifference / 5);
+      p2Pot = 2;
+    } else if (p2Points > p1Points) {
+      pointDifference = p2Points - p1Points;
+      p2Pot = 2 + 1 * Math.floor(pointDifference / 5);
+      p1Pot = 2;
+    } else {
+      p1Pot = 2;
+      p2Pot = 2;
+    }
 
     // Differanse (finne vinner)
     if (player1Points > player2Points) {
-        winner = 1;
-        difference = player1Points - player2Points;
-        p1Points = calculatePoints(difference, winner);
-        p2Points = calculatePoints(difference, winner);
+      setPlayer1GainedPoints(p2Pot);
+      setPlayer2GainedPoints(-p2Pot);
     } else if (player2Points > player1Points) {
-        winner = 2;
-        difference = player2Points - player1Points;
-        p1Points = calculatePoints(difference, winner);
-        p2Points = calculatePoints(difference, winner);
+      setPlayer1GainedPoints(-p1Pot);
+      setPlayer2GainedPoints(p1Pot);
     } else {
-        console.log("Uavgjort?")
+      console.log("Uavgjort?");
     }
-
-    // Oppdatere poeng de kommer til å tjene
-    setPlayer1GainedPoints(p1Points);
-    setPlayer2GainedPoints(p2Points);
   };
-
-  // Poengberegning for hver player 1p5
-  const calculatePoints = (difference, winner) => {
-    let points;
-
-    if (difference <= 5) {
-
-    } else if (difference <= 10) {
-        
-    }
-
-    return points;
-  }
 
   return (
     <div>
